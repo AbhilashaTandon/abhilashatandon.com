d33ac8528d64aea33a7d794ffbbc869cbb502824
Abhilasha Tandon <abhilashatandon167@gmail.com>
2023-08-23 01:24:53 -0400

    Worked on ContentGrid
    Added hover text
    Fixed borders
    TODO: Increase hover text font size

--- components/content-box.js
+++ components/content-box.js
@@ -1,18 +1,22 @@
-const classNames = require('classnames');
+const classNames = require("classnames");
 
 import Image from "next/image";
 import Styles from "@/styles/ContentGrid.module.css";
-import General from "@/styles/General.module.css"
+import General from "@/styles/General.module.css";
+import Link from "next/link";
 
-export default function ContentBox({ image, title, desc, link }) {
-  
-  var classnames = classNames(Styles.box, General.border_style);
-  //box that previews article or project
-  return (
-    <div className={classnames}>
-      <Image src={image} alt="error" className={Styles.img}/>
-      <div className={General.heading}>{title}</div>
-      <div className={General.body}>{desc}</div>
-    </div>
-  );
+export default function ContentBox({ image, title, desc, dest }) {
+    var classnames = classNames(Styles.box, General.border_style);
+    //box that previews article or project
+    return (
+        <>
+            <Link className={classnames} href={dest}>
+                <Image src={image} alt="error" className={Styles.img} />
+                <div className={Styles.hover_text}>
+                    <div className={General.heading}>{title}</div>
+                    <div className={General.heading}>{desc}</div>
+                </div>
+            </Link>
+        </>
+    );
 }
