a513c39f63c9c25077a50f5209fb485b884d0a61
Abhilasha Tandon <abhilashatandon167@gmail.com>
2024-06-16 23:38:12 -0400

    Wrote Navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Styles from "../styles/navbar.module.css";
+
+export default function Navbar() {
+    return (
+        <div className={Styles.navbar}>
+            <div className={Styles.logo}>
+                <Heading route="" name="ABHILASHATANDON.com" />
+            </div>
+            <div></div>
+            <div className={Styles.section}>
+                <Heading route="" name="Skills" />
+            </div>
+            <div className={Styles.section}>
+                <Heading route="" name="Projects" />
+            </div>
+            <div className={Styles.section}>
+                <Heading route="" name="Blog" />
+            </div>
+            <div className={Styles.button}>
+                <Heading route="" name="Resume" />
+            </div>
+        </div>
+    )
+}
+
+function Heading({ route, name }: { route: string; name: string }) {
+    return (
+        <Link href={route}>
+            {name}
+        </Link>
+    )
+} 
