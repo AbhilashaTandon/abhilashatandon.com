ab4820a98fb825b32bd3e4a1dbd2df1268453d8d
Abhilasha Tandon <abhilashatandon167@gmail.com>
2024-07-03 23:34:59 -0400

    Fixed file import error that prevented website from loading

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,29 +1,24 @@
 import styles from "../page.module.css";
-import NavStyle from "../../styles/navbar.module.css"
-import customStyling from "styles.css"
+import NavStyle from "../../styles/navbar.module.css";
+//import customStyling from "style.css";
 
 import Projects from "@/components/projects";
 
 import Link from "next/link";
 
 export default function Page() {
-    return (
-        <>
-            <nav className={NavStyle.logo}>
-                <Heading route="/" name="ABHILASHATANDON.com" />
-            </nav>
-            <main className={styles.page}>
-                <Projects />
-            </main>
-        </>
-    );
+  return (
+    <>
+      <nav className={NavStyle.logo}>
+        <Heading route="/" name="ABHILASHATANDON.com" />
+      </nav>
+      <main className={styles.page}>
+        <Projects />
+      </main>
+    </>
+  );
 }
 
-
 function Heading({ route, name }: { route: string; name: string }) {
-    return (
-        <Link href={route}>
-            {name}
-        </Link>
-    )
-} 
+  return <Link href={route}>{name}</Link>;
+}
