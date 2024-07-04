768efa03fadc49709d4b601bd2f7f2d409b05ce6
Abhilasha Tandon <abhilashatandon167@gmail.com>
2024-07-03 23:38:17 -0400

    fixed type error

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -6,77 +6,94 @@ import Image from "next/legacy/image";
 import Styles from "../styles/projects.module.css";
 import Link from "next/link";
 
-import TextStyles from "../styles/text.module.css"
+import TextStyles from "../styles/text.module.css";
 
 const projectsDir: string = "..//abhilashatandon.com//src//app//projects";
 
-async function getProjects(): Promise<{ project_name: string; image: string }[]> {
-  const projectPaths: string[] = (await fs.readdir(projectsDir, { withFileTypes: true })).filter(dirent => dirent.isDirectory()).map(dirent => dirent.name);
+async function getProjects(): Promise<
+  { project_name: string; image: string }[]
+> {
+  const projectPaths: string[] = (
+    await fs.readdir(projectsDir, { withFileTypes: true })
+  )
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
 
-  const projects: { project_name: string; image: string }[] = projectPaths.map((project) => {
-    const image: string = "/../../public/project_icons/" + project + ".png";
+  const projects: { project_name: string; image: string }[] = projectPaths.map(
+    (project) => {
+      const image: string = "/../../public/project_icons/" + project + ".png";
 
-    return { project_name: project, image: image };
-  });
+      return { project_name: project, image: image };
+    }
+  );
 
   return projects;
 }
 
 function capFirst(str: string) {
   str = str.replaceAll("-", " ");
-  return str.split(" ").map((word => {
-    return word[0].toUpperCase() + word.substring(1) + ' ';
-  }
-  ))
-
+  return str.split(" ").map((word) => {
+    return word[0].toUpperCase() + word.substring(1) + " ";
+  });
 }
 
-function ProjectTile({ project_name, box_style }: { project_name: string; box_style: string }): JSX.Element {
+function ProjectTile({
+  project_name,
+  box_style,
+}: {
+  project_name: string;
+  box_style: string;
+}): JSX.Element {
   return (
     <Link href={"/projects/" + project_name}>
       <div className={box_style}>
         <Image
-          src={require('./../../public/project-icons/' + project_name + '.png')}
+          src={require("./../../public/project-icons/" + project_name + ".png")}
           alt={project_name}
           layout="fill"
           objectFit="cover"
-          fill={true}
           style={{ maxHeight: "100%" }}
         />
-        <h1 className={Styles.projectName}>{
-          capFirst(project_name)}</h1>
+        <h1 className={Styles.projectName}>{capFirst(project_name)}</h1>
       </div>
-
     </Link>
   );
 }
 
 //makes a nice fancy asymmetric grid
-async function ProjectGrid({ project_data, parity }: { project_data: { project_name: string; }[]; parity: boolean }) {
-  const projectTiles = project_data.map((project: { project_name: string; }, idx: number) => {
-    var style: string = "";
-    switch (idx % 4) {
-      case 0:
-        style = parity ? Styles.bigBox : Styles.smallBox;
-        break;
-      case 1:
-        style = parity ? Styles.smallBox : Styles.bigBox;
-        break;
-      case 2:
-        style = parity ? Styles.smallBox : Styles.bigBox;
-        break;
-      case 3:
-        style = parity ? Styles.bigBox : Styles.smallBox;
-        break;
+async function ProjectGrid({
+  project_data,
+  parity,
+}: {
+  project_data: { project_name: string }[];
+  parity: boolean;
+}) {
+  const projectTiles = project_data.map(
+    (project: { project_name: string }, idx: number) => {
+      var style: string = "";
+      switch (idx % 4) {
+        case 0:
+          style = parity ? Styles.bigBox : Styles.smallBox;
+          break;
+        case 1:
+          style = parity ? Styles.smallBox : Styles.bigBox;
+          break;
+        case 2:
+          style = parity ? Styles.smallBox : Styles.bigBox;
+          break;
+        case 3:
+          style = parity ? Styles.bigBox : Styles.smallBox;
+          break;
+      }
+      return (
+        <ProjectTile
+          key={project.project_name}
+          project_name={project.project_name}
+          box_style={style}
+        />
+      );
     }
-    return (
-      <ProjectTile
-        key={project.project_name}
-        project_name={project.project_name}
-        box_style={style}
-      />
-    );
-  });
+  );
   return <div className={Styles.projectGrid}>{projectTiles}</div>;
 }
 
@@ -95,21 +112,16 @@ export default async function Projects() {
   }
   return (
     <div className={Styles.projects} id="projects">
-      <h2 className={TextStyles.section_header}
-        style={{ color: "var(--text-color)" }}>
-        <Link href="/projects">
-          My Projects
-        </Link>
-
+      <h2
+        className={TextStyles.section_header}
+        style={{ color: "var(--text-color)" }}
+      >
+        <Link href="/projects">My Projects</Link>
       </h2>
       <div className={Styles.project_grids}>
-        <ProjectGrid
-          project_data={oddProjects}
-          parity={false} />
-        <ProjectGrid
-          project_data={evenProjects}
-          parity={true} />
+        <ProjectGrid project_data={oddProjects} parity={false} />
+        <ProjectGrid project_data={evenProjects} parity={true} />
       </div>
-    </div >
+    </div>
   );
 }
