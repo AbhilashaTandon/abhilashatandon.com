30f7d57147f5ebde03e358acdf1e4b0722da541c
Abhilasha Tandon <abhilashatandon167@gmail.com>
2024-07-08 01:13:42 -0400

    Added youtube logo svg for when I make a youtube channel and updated avatar svg

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,39 +1,37 @@
 import Styles from "../styles/hero.module.css";
-import Avatar from "../../public/avatar.png"
-import Image from "next/legacy/image"
-
+import Avatar from "../../public/avatar.png";
+import Image from "next/legacy/image";
 
 export default function Hero() {
-    return (
-        <div className={Styles.hero} id="hero">
-            <Intro />
-            <Icon />
-        </div>
-    )
+  return (
+    <div className={Styles.hero} id="hero">
+      <Intro />
+      <Icon />
+    </div>
+  );
 }
 
 function Intro() {
-    return (
-        <div className={Styles.intro}>
-            <h3>Hi There 🙋🏽‍♀️! I’m</h3>
-            <h1 style={{ color: "var(--primary-color)" }}>Abhilasha Tandon.</h1>
-            <h4>I like making things. Designing things.
-                Coding things. Creating things.</h4>
-        </div>
-
-    )
+  return (
+    <div className={Styles.intro}>
+      <h3>Hi There 🙋🏽‍♀️! I’m</h3>
+      <h1 style={{ color: "var(--primary-color)" }}>Abhilasha Tandon.</h1>
+      <h4>
+        I like making things. Designing things. Coding things. Creating things.
+      </h4>
+    </div>
+  );
 }
 
 function Icon() {
-    return (
-        <div className={Styles.image_container}>
-            <Image
-
-                src={Avatar}
-                className={Styles.image}
-                layout="fill"
-
-                alt="Picture of the author" />
-        </div>
-    )
+  return (
+    <div className={Styles.image_container}>
+      <Image
+        src={Avatar}
+        className={Styles.image}
+        layout="fill"
+        alt="Picture of the author"
+      />
+    </div>
+  );
 }
