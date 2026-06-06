7fc1e27b777ef0f670c33d35d33b4efb70403c73
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-06-06 19:02:53 -0400

    Fixed merge issues and package.json formatting error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,5 @@
-import { toggleDarkMode } from "./darkMode.js"
 
 // https://stackoverflow.com/questions/56393880/how-do-i-detect-dark-mode-using-javascript
-
 
 import toggleDarkMode from "./darkMode";
 import createVis from "./spotify_artists";
