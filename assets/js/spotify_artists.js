53cec2724b32f4362b9e3fb6d0ab4fc81e9d76bd
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-06-09 20:43:47 -0400

    Improved text and bubble scaling, and disabled scroll and text selection

--- assets/js/spotify_artists.js
+++ assets/js/spotify_artists.js
@@ -1,6 +1,9 @@
 import * as d3 from "d3";
 import data from "../../collections/artist_data.json"
 
+var initial_zoom_level = .8;
+
+const scale_factor = .9;
 
 const sorted_data = data.sort((a, b) => a.popularity < b.popularity);
 // Final colors: "#005151", "#e6ac07", "#bc8dff", "#00bbff", "#ff9488", "#6c4700", "#64364d", "#3f9818",
@@ -9,15 +12,18 @@ const x_offset = 0;
 const y_offset = 0;
 
 function artists_shown(transform) {
+  // if (transform.k > 38) {
+  //   return sorted_data;
+  // }
   let shown = [];
 
-  const top = (-0.3 - transform.y) / transform.k;
+  const top = (-1 - transform.y) / transform.k;
   const bottom = (9.1 - transform.y) / transform.k;
   const left = (-6.8 - transform.x) / transform.k;
   const right = (15 - transform.x) / transform.k;
 
-  const x_threshold = 1.6 / transform.k;
-  const y_threshold = .2 / transform.k;
+  const x_threshold = 1.7 / Math.pow(transform.k, 1.0);
+  const y_threshold = .2 / Math.pow(transform.k, 1.0);
 
   for (const artist of sorted_data) {
     if (artist.x < left) {
@@ -49,43 +55,31 @@ function artists_shown(transform) {
   return shown;
 }
 
-function spatial_hash_max(data, scale) {
-  //
-  const cell_size_x = 2.5 / scale;
-  const cell_size_y = .5 / scale;
-
-  let hash_table = {};
-
-  for (const artist of data) {
-    const cell = [Math.floor(artist.x / cell_size_x), Math.floor(artist.y / cell_size_y)];
-    if (cell in hash_table) {
-      if (hash_table[cell].popularity < artist.popularity) {
-        hash_table[cell] = artist;
-      }
-    }
-    else {
-      hash_table[cell] = artist;
-    }
-
-  }
-
-  return Object.values(hash_table);
-
-}
-
 
 function handleZoom(event) {
   const { k, x, y } = event.transform;
 
+
+  // zoom_level = k;
+  console.log(k, x, y);
+
+  const scale_factor = Math.max(Math.sqrt(k), .9);
+
+
   const points = d3.selectAll("circle");
   const labels = d3.selectAll("text");
   const max_artists = artists_shown(event.transform);
-  points.data(data).attr("r", d => Math.sqrt(d.popularity / k) / (200));
+
+  points.data(data).attr("r", (d) => {
+    return Math.sqrt(d.popularity) / 200 / scale_factor;
+  });
+
   labels.data(data).attr("font-size", (d) => {
     if (!max_artists.includes(d)) {
       return 0;
     }
-    return .2 / k;
+    return Math.max(.035 * Math.pow(Math.log(d.popularity), 1 / Math.pow(scale_factor, .75)) / scale_factor, 0.002);
+
   });
 
 
@@ -95,8 +89,19 @@ function handleZoom(event) {
 }
 
 export default function createVis() {
+
+  // console.log(zoom_level);
+  const zoom = d3.zoom().scaleExtent([.8, 40]);
+
   const svg = d3.select("#spotify_artists")
-    .attr("viewBox", [-1, -1, 10, 10]).style('width', '100vw').style('height', '100vh');
+    .attr("viewBox", [-1, -1, 10, 10]).style('width', '100vw').style('height', '100vh')
+    // .call(zoom) // here
+    // .call(zoom.transform, d3.zoomIdentity.translate(.37486, 4.0352))
+    // .append("svg:g")
+    // .attr("transform", "translate(.37486,4.0352) ");
+    ;
+
+
 
 
   const points_group = svg.append("g").attr('opacity', '.6');
@@ -108,12 +113,14 @@ export default function createVis() {
     .attr("cx", (d => d.x + x_offset))
     .attr("cy", (d => d.y + y_offset))
     .attr('fill', "rgba(0, 0, 0, .25)")
-    .attr("r", (d => Math.sqrt(d.popularity) / 200))
+    // .attr("r", (d => Math.sqrt(d.popularity) / 200))
     .append("title")
     .text((d) => d.artist);
 
 
   const labels_group = svg.append('g');
+
+  console.log(zoom.append)
 
   const labels = labels_group.selectAll('text')
     .enter()
@@ -122,10 +129,11 @@ export default function createVis() {
     .text(d => d.artist)
     .attr('x', d => d.x + x_offset)
     .attr('y', d => d.y + y_offset)
-    .attr('font-size', '0.001em')
-    .attr('text-anchor', 'middle');
+    // .attr('font-size', d => String(0.001 * d.popularity) + 'em')
+    // .attr('opacity', d => String(100 - 500 / d.popularity + zoom_level) + '%')
+    .attr('text-anchor', 'middle')
+    .classed('noselect', true);
 
-  const zoom = d3.zoom().scaleExtent([.8, 40]);
   // .extent([[marginLeft, 0], [width - marginRight, height]])
   // .translateExtent([[marginLeft, -Infinity], [width - marginRight, Infinity]]);
 
@@ -135,7 +143,7 @@ export default function createVis() {
 
   svg
     .call(zoom)
-    .call(zoom.transform, d3.zoomIdentity)
+    .call(zoom.transform, d3.zoomIdentity.translate(1.4, 3).scale(initial_zoom_level))
 
 
 }
