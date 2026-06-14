4fe01a4b2cc3dc1e7a5b2d26a0a03d94e7fe290b
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-06-14 19:25:01 -0400

    Updated coordinate systems to be more intuitive and fixed artist finder

--- assets/js/spotify-vis.js
+++ assets/js/spotify-vis.js
@@ -1,4 +1,6 @@
 import updateVis from "./update-spotify-vis.js"
+import data from "../../collections/artist_data.json"
+let sorted_data = data.sort((a, b) => a.popularity < b.popularity);
 
 export default function createVis() {
         let canvas = document.getElementById("spotify-canvas");
@@ -7,22 +9,24 @@ export default function createVis() {
         }
         let ctx = canvas.getContext('2d');
 
-        let transform = { x: 0, y: 0, zoom: 1 }
+        let transform = { x: 440, y: 224, zoom: 72 }
 
         const MIN_ZOOM = .8;
 
         const MAX_ZOOM = 100;
 
-        const MIN_X = -222400
-        const MAX_X = -9400
-        const MIN_Y = -68000
-        const MAX_Y = 300
+        // const MIN_X = -222400
+        // const MAX_X = -9400
+        // const MIN_Y = -68000
+        // const MAX_Y = 300
 
         // Object { x: -4888.101146055941, y: -14830.411183743961, zoom: 15.490802233011848 }
         //Object { x: -92610.05706844841, y: 258.7759369478374, zoom: 101.57604483928662 }
         //Object { x: -1247142.405891415, y: -379265.441151771, zoom: 816.7466642802924 }
 
         let rect = canvas.getBoundingClientRect()
+        canvas.height = rect.height
+        canvas.width = rect.width
         // console.log(rect.width, rect.height)
 
         const centerX = rect.left + rect.width / 2
@@ -30,6 +34,8 @@ export default function createVis() {
 
         canvas.addEventListener('resize', (e) => {
                 rect = canvas.getBoundingClientRect()
+                canvas.height = rect.height
+                canvas.width = rect.width
         });
 
         //adapted from https://harrisonmilbradt.com/blog/canvas-panning-and-zooming
@@ -39,8 +45,12 @@ export default function createVis() {
         let previousX = 0,
                 previousY = 0
 
+        function getPosXY(x, y) {
+                return [(x - rect.left), (y - rect.top)]
+        }
+
         const getPos = (e) => {
-                return [((e.clientX - centerX) * 1.40 + centerX + 41) * (1440 / rect.width) - (1440 - rect.width) * .05, ((e.clientY - centerY) * 1.39 + centerY + 66.5) * (721 / rect.height) - (721 - rect.height) * .66]
+                return getPosXY(e.clientX, e.clientY)
         }
 
         const updatePanning = (e) => {
@@ -120,5 +130,83 @@ export default function createVis() {
         }
 
         canvas.addEventListener('wheel', onMouseWheel)
+
+
+
+        const search = document.querySelector('#search-bar')
+        const suggestions = document.querySelector('#search-suggestions')
+
+
+        search.addEventListener(
+                "input",
+                (event) => handleSearch(event, search, suggestions),
+
+        );
+
+        search.addEventListener('keydown', (event) => {
+                // Check if the pressed key is "Enter"
+                if (event.key === 'Enter') {
+                        event.preventDefault(); // Optional: Prevent default behavior (e.g., form submission)
+                        const new_transform = zoomToArtist(event.target.value.toLowerCase())
+                        if (new_transform != null) {
+                                transform = new_transform
+                                updateVis(ctx, canvas, transform)
+                                console.log(transform)
+                        }
+                }
+        });
+
+        function handleSearch(event, search, suggestions) {
+                const query = event.target.value.toLowerCase();
+                while (suggestions.lastElementChild) {
+                        suggestions.removeChild(suggestions.lastElementChild);
+                }
+
+                let suggested_artists = []
+
+                for (const artist of sorted_data) {
+                        const artist_name = artist.artist.toLowerCase()
+                        if (!artist_name.includes(query)) {
+                                continue;
+                        }
+
+                        suggested_artists.push(artist)
+                        if (suggested_artists.length == 5) {
+                                break;
+                        }
+
+                }
+
+
+                for (const suggestion of suggested_artists) {
+                        const suggestion_tag = document.createElement("option")
+                        suggestion_tag.setAttribute("value", suggestion.artist)
+                        suggestions.appendChild(suggestion_tag)
+                }
+
+
+
+        }
+
+        function zoomToArtist(artist_name) {
+                const selected_artist = sorted_data.find((artist) => artist.artist.toLowerCase() === artist_name)
+
+                if (selected_artist == null) {
+                        return null
+                }
+
+                const zoom_level = 5000
+
+                const x_coord = selected_artist.x * zoom_level - rect.width / 2
+                const y_coord = selected_artist.y * zoom_level - rect.height / 2
+
+                const target_transform = { x: -x_coord, y: -y_coord, zoom: zoom_level }
+                console.log(target_transform)
+
+                return target_transform
+        }
+
+
+
 }
 
--- assets/js/update-spotify-vis.js
+++ assets/js/update-spotify-vis.js
@@ -5,18 +5,24 @@ let sorted_data = data.sort((a, b) => a.popularity < b.popularity);
 function drawCircle(ctx, x, y, radius, color) {
         ctx.beginPath();
         ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-        const root = document.documentElement;
         ctx.fillStyle = color;
         ctx.fill();
 }
 
-const x_threshold = 0.15
-const y_threshold = .025
+const x_threshold = 3000
+const y_threshold = 430
+
+function artist_coord_to_local_coord(x, y) {
+        return [x
+                , y]
+}
 
 export default function updateVis(ctx, canvas, transform) {
 
+        console.log(transform)
         ctx.setTransform(1, 0, 0, 1, 0, 0)
         ctx.fillStyle = "#ffffff"
+        //clear screen
         ctx.fillRect(0, 0, canvas.width, canvas.height)
         ctx.transform(
                 transform.zoom,
@@ -33,43 +39,50 @@ export default function updateVis(ctx, canvas, transform) {
         const top = -transform.y / transform.zoom - 1
         const right = (2000 - transform.x) / transform.zoom + 1
         const bottom = (1000 - transform.y) / transform.zoom + 1
+        //bounds of screen in local coordinate system
 
 
 
         sorted_data.map((artist) => {
-                const x_coord = 100 * artist.x + 600
-                const y_coord = 100 * artist.y + 300
-                const radius = .75 * Math.sqrt(artist.popularity) / Math.pow(transform.zoom, .6)
+                const [x_coord, y_coord] = artist_coord_to_local_coord(artist.x, artist.y)
+                const radius = .08 * Math.sqrt(artist.popularity) / Math.pow(transform.zoom, .5)
 
                 if (x_coord < left || x_coord > right || y_coord < top || y_coord > bottom) {
                         return
                 }
 
-                drawCircle(ctx, x_coord, y_coord, radius, "#69696920")
+                const opacity = Math.min(Math.max(Math.floor(Math.sqrt(transform.zoom) * 2), 16), 64)
+
+
+                drawCircle(ctx, x_coord, y_coord, radius, "#696969" + opacity.toString(16))
 
 
         });
 
         //I need to do go through the artists twice so that the circles dont cover the labels
+        //
+        ctx.fillStyle = "#000000ff";
+        ctx.font = "72px serif";
+        const max_artists = 100
         let labeled_artists = []
         sorted_data.map((artist) => {
-                const x_coord = 100 * artist.x + 600
-                const y_coord = 100 * artist.y + 300
-                const size = 6 * Math.pow(artist.popularity / transform.zoom + 2, .2) / Math.pow(transform.zoom, .7)
+                const [x_coord, y_coord] = artist_coord_to_local_coord(artist.x, artist.y)
+                const size = 0.023 * Math.pow(artist.popularity / Math.pow(transform.zoom, 1) + 2, .55) / Math.pow(transform.zoom, .75)
+
                 const threshold = size / Math.pow(transform.zoom, .25)
 
 
                 if (x_coord < left || x_coord > right || y_coord < top || y_coord > bottom) {
                         return
                 }
+                //outside bounds
 
-
-
-
-
+                ctx.scale(size, size)
                 let overlap = false
 
                 for (const labeled_artist of labeled_artists) {
+
+
                         const x_distance = Math.abs(artist.x - labeled_artist.x)
                         const y_distance = Math.abs(artist.y - labeled_artist.y)
                         if (x_distance < x_threshold * threshold && y_distance < y_threshold * threshold) {
@@ -80,53 +93,22 @@ export default function updateVis(ctx, canvas, transform) {
                 }
 
                 if (!overlap) {
-                        ctx.fillStyle = "#000000ff";
-                        ctx.font = size + "px serif";
-                        ctx.fillText(artist.artist, x_coord, y_coord)
+
+                        var metrics = ctx.measureText(artist.artist);
+                        var textWidth = metrics.width;
+                        ctx.fillText(artist.artist, x_coord / size - textWidth / 2, y_coord / size)
                         labeled_artists.push(artist)
                 }
+                ctx.scale(1 / size, 1 / size)
         });
 
 
 
-        const search = document.querySelector('#search-bar')
-        const suggestions = document.querySelector('#search-suggestions')
 
-
-        search.addEventListener(
-                "input",
-                (event) => {
-                        const query = event.target.value.toLowerCase();
-                        while (suggestions.lastElementChild) {
-                                suggestions.removeChild(suggestions.lastElementChild);
-                        }
-
-                        let suggested_artists = []
-
-                        for (const artist of sorted_data) {
-                                const artist_name = artist.artist.toLowerCase()
-                                if (artist_name.includes(query)) {
-                                        suggested_artists.push(artist.artist)
-                                        if (suggested_artists.length == 5) {
-                                                break;
-                                        }
-                                }
-
-                        }
-
-
-                        for (const suggestion of suggested_artists) {
-                                const suggestion_tag = document.createElement("option")
-                                suggestion_tag.setAttribute("value", suggestion)
-                                suggestions.appendChild(suggestion_tag)
-                        }
-                },
-
-        );
-
-//left -x / z
-//top -y / z
-//right (2000 - x) / z
-//bottom (1000 - y) / z
+        //left -x / z
+        //top -y / z
+        //right (2000 - x) / z
+        //bottom (1000 - y) / z
 
 }
+
