a1e23418fb9bffa73c4c48e49add6817e8cc6a2a
Abhilasha Tandon <abhilashatandon167@gmail.com>
2026-08-04 21:02:46 -0400

    Added timer element and modularized some code into methods

--- assets/js/keylie.js
+++ assets/js/keylie.js
@@ -1,26 +1,5 @@
-function loadSample() {
-        fetch('https://www.gutenberg.org/cache/epub/345/pg345.txt')
-                .then(response => response.text())
-                .then((data) => {
-                        console.log(data)
-                })
-}
 
-
-export default function keylie() {
-        loadSample();
-        let root = document.getElementById("Keylie");
-
-        if (root == null) {
-                console.log("Root element not found.");
-                return;
-        }
-
-        let textbox = document.getElementById("text");
-
-        const text = "The quick brown fox jumps over the lazy dog.";
-
-        const cursor = document.createElement("h5");
+function renderText(cursor, text, textbox) {
         cursor.classList.add("w-max");
         cursor.classList.add("text-4xl");
         const char_elem = document.createTextNode("⏐");
@@ -29,22 +8,38 @@ export default function keylie() {
 
         for (let i = 0; i < text.length; i++) {
                 const child = document.createElement("h5");
-                child.classList.add("w-max");
-                child.classList.add("text-4xl");
+                // child.classList.add("w-max");
+                child.classList.add("text-[2rem]");
                 child.classList.add("font-medium");
                 child.classList.add("text-(--gray)");
+
                 let char = text[i];
                 if (char === " ") {
-                        char = '\xa0';
-
+                        char = ' ';
+                        child.classList.add("w-[0.5rem]")
                 }
+
                 const char_elem = document.createTextNode(char);
                 child.appendChild(char_elem);
                 textbox.appendChild(child);
         }
+}
+
+let root = document.getElementById("Keylie");
 
 
-        let cursor_pos = 0;
+export default function keylie() {
+
+        function reset() {
+                cursor_pos = 0;
+                textbox.insertBefore(cursor, textbox.children[0]);
+                for (let char_idx = 1; char_idx < textbox.children.length; char_idx++) {
+                        const current_char_elem = textbox.children[char_idx]
+                        current_char_elem.classList.remove("text-(--accent)")
+                        current_char_elem.classList.remove("text-red-500")
+                        current_char_elem.classList.add("text-(--gray)")
+                }
+        }
 
         function onKeypress(event) {
                 console.log(event)
@@ -66,12 +61,14 @@ export default function keylie() {
 
                 const current_char = text[cursor_pos - 1];
                 if (current_char != event.key) {
-                        current_char_elem.classList.add("text-red-500")
+                        reset();
+                        // current_char_elem.classList.add("text-red-500")
                 }
                 else {
-                        current_char_elem.classList.add("text-(--text)")
+                        current_char_elem.classList.add("text-(--accent)")
                 }
         }
+
 
         function onKeydown(event) {
                 textbox.focus();
@@ -89,12 +86,39 @@ export default function keylie() {
                 textbox.insertBefore(cursor, textbox.children[cursor_pos]);
 
                 const current_char_elem = textbox.children[cursor_pos + 1]
-                current_char_elem.classList.remove("text-(--text)")
+                current_char_elem.classList.remove("text-(--accent)")
                 current_char_elem.classList.remove("text-red-500")
                 current_char_elem.classList.add("text-(--gray)")
 
         }
 
+        if (root == null) {
+                console.log("Root element not found.");
+                return;
+        }
+
+        let textbox = document.getElementById("text");
+        let timer_elem = document.getElementById("timer");
+
+        const text = "The quick brown fox jumps over the lazy dog.";
+        const desired_speed = 10; //wpm
+        const time_for_text = text.length / 5 / desired_speed * 60;
+
+        const cursor = document.createElement("h5");
+        renderText(cursor, text, textbox)
+
+        let cursor_pos = 0;
+        reset();
+
         addEventListener("keypress", onKeypress);
         addEventListener("keydown", onKeydown);
+
+        let timer = time_for_text;
+
+        function decrementTimer() {
+                timer_elem.innerText = Math.round(timer);
+                timer -= 1;
+        }
+
+        setTimeout(decrementTimer(), 1)
 }
